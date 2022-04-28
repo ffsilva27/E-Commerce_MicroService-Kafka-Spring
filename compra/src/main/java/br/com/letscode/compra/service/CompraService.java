e36a82acf97700415fa3efa7e020ac2e02281700
Vitor Zillig <vitorzillig.azapfy@gmail.com>
2022-04-27 22:04:14 -0300

    Kafka fixed flow

--- compra/src/main/java/br/com/letscode/compra/service/CompraService.java
+++ compra/src/main/java/br/com/letscode/compra/service/CompraService.java
@@ -3,6 +3,7 @@ package br.com.letscode.compra.service;
 import br.com.letscode.compra.dto.CompraRequest;
 import br.com.letscode.compra.dto.CompraResponse;
 import br.com.letscode.compra.exceptions.BadRequest;
+import br.com.letscode.compra.kafka.SendKafkaMessage;
 import br.com.letscode.compra.model.*;
 import br.com.letscode.compra.repository.CompraProdutoRepository;
 import br.com.letscode.compra.repository.CompraRepository;
@@ -24,6 +25,7 @@ public class CompraService {
 
     private final CompraRepository compraRepository;
     private final CompraProdutoRepository compraProdutoRepository;
+    private final SendKafkaMessage sendKafkaMessage;
 
     public Page<CompraResponse> listByCPF(String cpf, Pageable pageable) {
         Specification<Compra> specification = Specification.where(null);
@@ -37,53 +39,55 @@ public class CompraService {
 
     @Transactional
     public CompraResponse createCompra(CompraRequest compraRequest) throws BadRequest {
-        double sum_values = 0.0;
-
+//        double sum_values = 0.0;
+//
         Compra compra = new Compra();
         compra.setData_compra(compraRequest.getData());
         compra.setCpf(compraRequest.getCpf());
         compra.setValor_total_compra(0.0);
 
-        compraRepository.save(compra);
-
-        for (Map.Entry<String,Integer> entry : compraRequest.getProdutos().entrySet()){
-            Produto produto = ProdutoService.getProduct(entry);
-            if (produto==null){
-                compraProdutoRepository.deleteAll(compra.getProdutos());
-                compraRepository.delete(compra);
-                throw new BadRequest("Produto não encontrado");
-            }
-            if (produto.getQtde_disponivel() < entry.getValue()) {
-                compraProdutoRepository.deleteAll(compra.getProdutos());
-                compraRepository.delete(compra);
-                throw new BadRequest("Quantidade indisponível do produto: " + produto.getNome());
-            }
-            CompraProdutoKey key = new CompraProdutoKey();
-            key.setIdCompra(compra.getId());
-            key.setIdProduto(produto.getId());
-
-            CompraProduto compraProduto = new CompraProduto();
-            compraProduto.setCompra(compra);
-            compraProduto.setProduto(produto);
-            compraProduto.setQuantidade(entry.getValue());
-            compraProduto.setCompraProdutoKey(key);
-
-            compraProdutoRepository.save(compraProduto);
-            compra.getProdutos().add(compraProduto);
-
-            sum_values += produto.getPreco()*entry.getValue();
-        }
-
-        ProdutoService.updateQuantity(compraRequest.getProdutos());
-        compra.setValor_total_compra(sum_values);
-
-        if(compra.getValor_total_compra() == null){
-            throw new BadRequest("O campo produtos deve ser preenchido.");
-        }
-
-        compraRepository.save(compra);
-
+//        compraRepository.save(compra);
+//
+//        for (Map.Entry<String,Integer> entry : compraRequest.getProdutos().entrySet()){
+//            Produto produto = ProdutoService.getProduct(entry);
+//            if (produto==null){
+//                compraProdutoRepository.deleteAll(compra.getProdutos());
+//                compraRepository.delete(compra);
+//                throw new BadRequest("Produto não encontrado");
+//            }
+//            if (produto.getQtde_disponivel() < entry.getValue()) {
+//                compraProdutoRepository.deleteAll(compra.getProdutos());
+//                compraRepository.delete(compra);
+//                throw new BadRequest("Quantidade indisponível do produto: " + produto.getNome());
+//            }
+//            CompraProdutoKey key = new CompraProdutoKey();
+//            key.setIdCompra(compra.getId());
+//            key.setIdProduto(produto.getId());
+//
+//            CompraProduto compraProduto = new CompraProduto();
+//            compraProduto.setCompra(compra);
+//            compraProduto.setProduto(produto);
+//            compraProduto.setQuantidade(entry.getValue());
+//            compraProduto.setCompraProdutoKey(key);
+//
+//            compraProdutoRepository.save(compraProduto);
+//            compra.getProdutos().add(compraProduto);
+//
+//            sum_values += produto.getPreco()*entry.getValue();
+//        }
+//
+//        ProdutoService.updateQuantity(compraRequest.getProdutos());
+//        compra.setValor_total_compra(sum_values);
+//
+//        if(compra.getValor_total_compra() == null){
+//            throw new BadRequest("O campo produtos deve ser preenchido.");
+//        }
+//
+//        compraRepository.save(compra);
+//
+        sendKafkaMessage.sendMessage(compraRequest);
         return CompraResponse.convert(compra);
+
     }
 
     public Produto teste(){
